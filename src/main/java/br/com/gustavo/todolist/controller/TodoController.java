47ddd8529dc3676481126511c1c2fae4e9af7e61
Guh-paixao <gustavopaixao.dev@gmail.com>
2025-03-25 18:56:38 -0300

    feat: configure PostgreSQL database and add TodoController for task management

---
+++ src/main/java/br/com/gustavo/todolist/controller/TodoController.java
@@ -0,0 +1,38 @@
+package br.com.gustavo.todolist.controller;
+
+
+import br.com.gustavo.todolist.entity.Todo;
+import br.com.gustavo.todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public List<Todo> createTodo(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    public List<Todo> listTodos() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    public List<Todo> updateTodo(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public List<Todo> deleteTodo(@PathVariable("id") Long id) {
+        return todoService.delete(id);
+    }
+}
