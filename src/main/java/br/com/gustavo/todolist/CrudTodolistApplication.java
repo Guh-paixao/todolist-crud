8c8cbfaf86558fb2b49f3a5496ba55976b9328ea
Guh-paixao <gustavopaixao.dev@gmail.com>
2025-03-25 17:46:24 -0300

    feat: initial commit

---
+++ src/main/java/br/com/gustavo/todolist/CrudTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTodolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTodolistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gustavo/todolist/CrudTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
